9f9520fe1328bfca63fbb77ca650479bf21fdf33
Luke Ku <lukeku@Lukes-MacBook-Pro-5.local>
2025-01-12 17:53:18 -0800

    Initial commit

--- main.py
+++ main.py
@@ -126,13 +126,3 @@ class DiceGame:
             print("You won the game!! You winner!")
             print("********************")
         
-
-player_die = Die()
-computer_die = Die()
-
-my_player = Player(player_die, is_computer=False)
-computer_player = Player(computer_die, is_computer=True)
-
-game = DiceGame(my_player, computer_player)
-
-game.play()
